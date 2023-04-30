304561467c94e7889217950bbe676fefedd68e10
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2023-04-30 14:06:24 +0700

    refresh design

--- pages/api/image.tsx
+++ pages/api/image.tsx
@@ -78,82 +78,107 @@ export default async function httpPos(req: NextRequest) {
       paddingTop: '10px',
       paddingBottom: '10px',
       display: 'flex',
-      backgroundColor: 'white',
+      alignItems: 'center',
+      backgroundColor: `#${tags[i].color}`,
       marginRight: '16px',
       marginBottom: '16px',
       borderRadius: '12px',
     }}>
       <span style={{
         fontFamily: 'Material Symbols Rounded',
-        color: `#${tags[i].color}`,
-        fontSize: '40px',
+        color: 'white',
+        fontSize: '36px',
       }}>{convertUnicode(tags[i].icon)}</span>
       <span style={{
         fontFamily: 'Roboto',
-        color: '#100f0f',
-        fontSize: '36px',
-        fontWeight: 'normal',
+        color: 'white',
+        fontSize: '24px',
+        fontWeight: 'semibold',
         marginLeft: '10px',
       }}>{tags[i].label}</span>
     </div>)
   }
 
+  const gradientColor = `linear-gradient(135deg, #${color} 0%, #${color.slice(0, -2)}ff 100%)`
+
   return new ImageResponse(
     (
-      <div style={{
-        backgroundColor: '#' + color,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        width: '100%',
-        height: '100%',
-        padding: '36px',
-      }}>
-        <div style={{
+      <div
+        style={{
+          background: gradientColor,
           display: 'flex',
-          flexDirection: 'row',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          width: '100%',
           height: '100%',
-        }}>
+          padding: '36px',
+          borderRadius: '24px',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            height: '100%',
+          }}
+        >
           <img
-            width='200'
-            height='200'
+            width='240px'
+            height='240px'
             src={avatar}
             style={{
-              borderRadius: 40,
+              borderRadius: '50%',
               objectFit: 'cover',
+              boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
             }}
           />
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            paddingLeft: '54px',
-            height: '100%',
-          }}>
-            <div style={{
+          <div
+            style={{
               display: 'flex',
               flexDirection: 'column',
-            }}>
-              <h1 style={{
-                color: 'white',
-                fontSize: '72px',
-                fontFamily: 'Roboto',
-                fontWeight: 'bold',
-              }}>{title}</h1>
-              <h3 style={{
-                color: 'white',
-                fontSize: '48px',
-                fontFamily: 'Roboto',
-                fontWeight: 'normal',
-              }}>{subtitle}</h3>
+              justifyContent: 'space-between',
+              paddingLeft: '54px',
+              height: '100%',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <h1
+                style={{
+                  color: 'white',
+                  fontSize: '72px',
+                  fontFamily: 'Roboto',
+                  fontWeight: 'bold',
+                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)',
+                }}
+              >
+                {title}
+              </h1>
+              <h3
+                style={{
+                  color: 'white',
+                  fontSize: '48px',
+                  fontFamily: 'Roboto',
+                  fontWeight: 'normal',
+                  textShadow: '1px 1px 3px rgba(0, 0, 0, 0.2)',
+                }}
+              >
+                {subtitle}
+              </h3>
             </div>
-            <div style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'flex-start',
-              flexWrap: 'wrap',
-              paddingRight: '180px',
-            }}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                flexWrap: 'wrap',
+                paddingRight: '180px',
+              }}
+            >
               {rows}
             </div>
           </div>
@@ -186,5 +211,6 @@ export default async function httpPos(req: NextRequest) {
     },
   )
 }
+
 
 
